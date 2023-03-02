88ea3b9b4845639b0210fcb114441433e83b6b5f
Joseph Yi <sendtojsph@gmail.com>
2023-03-01 21:35:21 -0500

    Initial Commit

---
+++ BetterRestV2/BetterRestV2App.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestV2App.swift
+//  BetterRestV2
+//
+//  Created by Joe Yi on 11/28/22.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestV2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
